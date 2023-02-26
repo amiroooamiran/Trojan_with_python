c1c124d6fe875dab934b883e848ce9756479387f
amirhossein amiran <93470832+amirhosseiamiran2004@users.noreply.github.com>
2023-02-27 00:29:03 +0330

    get the local IP from Host

--- Trojan.py
+++ Trojan.py
@@ -1,14 +1,19 @@
 # Import Section
 import pygame
 import platform
+import socket
 
 # pygame init and create font for text
 pygame.init()
 screen = pygame.display.set_mode((500, 300))
 textfont = pygame.font.SysFont("", 50)
 
+# geting Oprating system
 os_info = platform.uname()
 
+# grting the local_Ip 
+hostname = socket.gethostname()
+ip_local = socket.grthostbyname(hostname)
 # color of the Screen
 RED = (255, 0, 0)
 running = True
@@ -19,6 +24,7 @@ while running:
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
             print(os_info)
+            print()
             running = False
     textTBD = textfont.render("I Love you baybe :)", 1, (255, 255, 255))
     screen.blit(textTBD, (100, 100))
