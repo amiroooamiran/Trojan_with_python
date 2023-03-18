437fe5f0d7b24c7733a61d9d1d252dbabe4733dc
amirhossein amiran <93470832+amirhosseiamiran2004@users.noreply.github.com>
2023-03-18 18:53:19 +0330

    geting public Ip

--- Trojan.py
+++ Trojan.py
@@ -3,6 +3,8 @@ import pygame
 import platform
 import socket
 
+import requests
+
 # pygame init and create font for text
 pygame.init()
 screen = pygame.display.set_mode((500, 300))
@@ -13,15 +15,27 @@ os_info = platform.uname()
 
 # grting the local_Ip 
 hostname = socket.gethostname()
-ip_local = socket.grthostbyname(hostname)
+ip_local = socket.gethostbyname(hostname)
 # color of the Screen
 RED = (255, 0, 0)
 running = True
+
+
+# geting public Ip 
+ret = True
+printed = False
+
+public_ip = requests.get('http://api.ipify.org/').text
 
 # Loop for runing the system
 while running:
     screen.fill(RED)
     for event in pygame.event.get():
+
+        if ret == True and not printed:
+            print('public Ip :' + public_ip) 
+            printed = True
+
         if(event.type == pygame.QUIT):
             print(os_info)
             print()
